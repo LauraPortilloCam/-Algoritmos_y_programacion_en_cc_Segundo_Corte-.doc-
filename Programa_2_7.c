48fc551a96033468f3d837d92a80e43ea059ff81
Liam <lsportilloc@udistrital.edu.co>
2025-10-24 13:33:17 -0500

    Add files via upload

---
+++ Programa_2_7.c
@@ -0,0 +1,18 @@
+//Programa7.2
+//Laura Portillo
+//24 de octubre del 2025
+//Funcionamiento de  las condiciones de if
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include<stdio.h>
+int  main()
+{
+ if(0)// es nuestra condición
+  {
+   printf("Esta instruccion nunca se ejecuta \n.");
+   printf("porque la condición siempre es falsa (0). \n");
+  }
+ if(-38)// Cuando ponemos un numero  diferente a certo se ejecuta el programa
+   printf("Esta instruicción siempre se ejecuta. \n");
+  return 0;
+}
